2a3af73fe7d4ff7d52c330b4821247aeb5c0d606
ghali <ghali.bnb02@gmail.com>
2026-05-13 18:42:14 +0200

    Added health FastAPI endpoint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from nexus!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
