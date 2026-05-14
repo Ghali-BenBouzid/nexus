089b083b8c7d8a22d89515ff41c1a310765d01a0
ghali <ghali.bnb02@gmail.com>
2026-05-14 15:53:58 +0200

    added databse user model

---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
