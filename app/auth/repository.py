09381de0b8f61b856b9d4074f639ebfb7bc23b46
ghali <ghali.bnb02@gmail.com>
2026-05-18 20:46:47 +0200

    fixed ruff lint errors

--- app/auth/repository.py
+++ app/auth/repository.py
@@ -1,14 +1,14 @@
-from app.models.user import User
 from sqlalchemy.orm import Session
+
+from app.models.user import User
+
 
 def get_user_by_email(db: Session, email: str) -> User | None:
     return db.query(User).filter(User.email == email).first()
 
+
 def create_user(db: Session, email: str, hashed_password: str) -> User:
-    user = User(
-        email=email,
-        hashed_password=hashed_password
-    )
+    user = User(email=email, hashed_password=hashed_password)
 
     db.add(user)
     db.commit()
